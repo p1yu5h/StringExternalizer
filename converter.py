c0197cb0f803ce2287665c959ac2d2ad0aca1dca
p1yu5h <piyush.satija@gmail.com>
2019-10-17 22:59:59 +0530

    add autoincrement id to each line.

--- converter.py
+++ converter.py
@@ -1,8 +1,10 @@
 f_in = open("sample.txt", "r")
 f_out = open("output.txt", "w+")
 keyword = input("Enter keyword: ")
+i = 0
 for line in f_in:
     if (line.strip()):
-        text = "<string name=\"" + keyword + "\">" + line.strip().lstrip() + "</string>\n"
+        i+=1
+        text = "<string name=\"" + keyword + str(i) + "\">" + line.strip().lstrip() + "</string>\n"
         f_out.write(text)
 print("Done!")
